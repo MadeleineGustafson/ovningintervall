0c1c9069e8ef3b91ca2bf9653072939f58239fd2
MadeleineGustafson <madeleinegu@gmail.com>
2022-11-15 09:41:46 +0100

    add first files

---
+++ main.js
@@ -0,0 +1,13 @@
+window.addEventListener('DOMContentLoaded', main)
+
+function main() {
+    //Start på programmet
+    setTimeout (changeBackgroundColor, 4000)
+
+}
+
+function changeBackgroundColor() {
+    document.body.style.background="lightblue"
+        
+}
+
