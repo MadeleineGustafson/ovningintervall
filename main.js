cb2be3cd42485759cc22c4217af88863a70b3b08
MadeleineGustafson <madeleinegu@gmail.com>
2022-11-15 09:55:37 +0100

    add button to cancel bg-change

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
-window.addEventListener('DOMContentLoaded', main)
+window.addEventListener('DOMContentLoaded', main);
+
+let timeoutRef;
 
 function main() {
     //Start på programmet
-    setTimeout (changeBackgroundColor, 4000)
+   timeoutRef= setTimeout (changeBackgroundColor, 4000);
+
+    addEventListeners ();
 
 }
 
 function changeBackgroundColor() {
     document.body.style.background="lightblue"
         
+}
+
+function addEventListeners() {
+    const buttton = document.getElementById ("cancel");  //gör knappen till variabel
+    buttton.addEventListener ('click', cancelBackgroundChange); 
+}
+
+function cancelBackgroundChange() {
+    clearTimeout(timeoutRef);
+
 }
 
